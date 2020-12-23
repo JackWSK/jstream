39a066a7e01f280fd7fad56f10d317eb9e4e1f55
284569022 <284569022@qq.com>
2020-12-23 15:59:11 +0800

    change

--- stream_test.go
+++ stream_test.go
@@ -2,7 +2,6 @@ package jstream
 
 import (
     "fmt"
-    "reflect"
     "testing"
 )
 
@@ -10,24 +9,24 @@ type User struct {
     name string
 }
 
-func Test_Array(t *testing.T) {
-    var users []*User
+func Test_ToArray(t *testing.T) {
+    var output []string
     FromArray([]*User{{name: "123"}, {name: "2222"}, {name: "123"}, {name: "123"}}).
         Filter(func(e interface{}) bool {
-            return e.(*User).name == "2222"
+            return e.(*User).name != "2222"
         }).
-        //Map(func(e interface{}) interface{} {
-        //   return fmt.Sprintf("%d", e)
-        //}).
-        Collect(ToArray(&users))
-    fmt.Println(users)
+        Map(func(e interface{}) interface{} {
+            return e.(*User).name
+        }).
+        Collect(ToArray(&output))
+    fmt.Println(output)
 }
 
 func Test_Map(t *testing.T) {
     var users map[string]*User
     FromArray([]*User{{name: "1234"}, {name: "2222"}, {name: "12"}, {name: "1235"}}).
         Filter(func(e interface{}) bool {
-           return e.(*User).name != "2222"
+            return e.(*User).name != "2222"
         }).
         //Map(func(e interface{}) interface{} {
         //   return fmt.Sprintf("%d", e)
@@ -41,24 +40,11 @@ func Test_Map(t *testing.T) {
 func Test_Group(t *testing.T) {
     var users map[string][]*User
     FromArray([]*User{{name: "2222"}, {name: "2222"}, {name: "12"}, {name: "1235"}}).
-        //Filter(func(e interface{}) bool {
-        //    return e.(*User).name != "2222"
-        //}).
-        //Map(func(e interface{}) interface{} {
-        //   return fmt.Sprintf("%d", e)
-        //}).
+        Filter(func(e interface{}) bool {
+            return e.(*User).name != "2222"
+        }).
         Collect(Group(&users, func(i interface{}) interface{} {
             return (i.(*User)).name
         }))
     fmt.Println(users)
-}
-
-func Test_MapKey(t *testing.T) {
-   var users map[string][]*User
-
-    value := reflect.ValueOf(users)
-    listValue := value.MapIndex(reflect.ValueOf("abc"))
-    if listValue.Kind() == reflect.Invalid {
-        listValue = reflect.MakeSlice(value.Type().Elem(), 0, 0)
-    }
 }
